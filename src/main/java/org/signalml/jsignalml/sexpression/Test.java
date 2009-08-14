a899a1332e15b935aaab80ad54cc6185b67bbba8
Zbyszek Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2009-08-15 00:43:34 +0200

    Primitive testing engine.

--- src/main/java/org/signalml/jsignalml/sexpression/Test.java
+++ src/main/java/org/signalml/jsignalml/sexpression/Test.java
@@ -6,8 +6,17 @@ import org.antlr.runtime.*;
 import org.antlr.runtime.tree.*;
 
 public class Test {
+    static class State implements CallHelper {
+	public Type call(java.lang.String id, Object ... args)
+	    throws ExpressionFault
+	{
+	    throw new ExpressionFault.NameError(id);
+	}
+    }
+
     public static void main(String[] args) throws Exception {
 	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	CallHelper state = new State();
 	while(true){
 	    String line = in.readLine();
 
@@ -16,15 +25,29 @@ public class Test {
 	    CommonTokenStream tokens = new CommonTokenStream(lexer);
 	    SExpressionParser parser = new SExpressionParser(tokens);
 	    SExpressionParser.singleexpr_return r = parser.singleexpr();
+	    if(r == null)
+		continue;
 
-	    System.out.println(r.tree.toStringTree());
+	    System.out.format("ast: %s\n", r.tree.toStringTree());
 	    System.out.print("--------------------\ntree: ");
-
 	    CommonTreeNodeStream nodes = new CommonTreeNodeStream(r.getTree());
 	    STree tree = new STree(nodes);
 	    Expression expr = tree.expr();
-	    System.out.println(expr);
+	    System.out.println(tree==null ? "(null)" : expr);
+	    if(tree == null)
+		continue;
+
+	    System.out.print("--------------------\nvalue: ");
+
+	    try{
+		Type val = expr.eval(state);
+		System.out.println(val == null ? "null" : val.repr());
+	    }catch(ExpressionFault e){
+		System.out.println(e);
+	    }
+
 	    System.out.println("--------------------");
+	    
 	}
     }
 }
