998619917cae6a8b0ff603f4ef93887e96e7adbc
Zbyszek Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2009-08-26 23:19:40 +0200

    Add .read() to BitForms and test.

--- src/main/java/org/signalml/jsignalml/BitForm.java
+++ src/main/java/org/signalml/jsignalml/BitForm.java
@@ -1,5 +1,8 @@
 package org.signalml.jsignalml;
 
+import org.signalml.jsignalml.sexpression.Type;
+import java.nio.ByteBuffer;
+
 public abstract class BitForm {
     public static class BadBitForm extends Exception {
 	public final String description;
@@ -14,14 +17,29 @@ public abstract class BitForm {
 	if(description.equals("<i1") ||
 	   description.equals(">i1") ||
 	   description.equals("|i1"))
-	    return new Byte();
+	    return new Int8();
 	if(description.equals("<i2"))
 	    return new Int32.LE();
 	throw new BadBitForm(description);
     }
 
-    public static class Byte extends BitForm {};
+    public abstract Type read(ByteBuffer buffer, int offset);
+
+    public static class Int8 extends BitForm {
+	@Override
+	public Type.Int read(ByteBuffer buffer, int offset){
+	    byte data = buffer.get(offset);
+	    return new Type.Int(data);
+	}
+    }
+
     public static abstract class Int32 extends BitForm {
-	public static class LE extends Int32 {}
+	public static class LE extends Int32 {
+	    @Override
+	    public Type.Int read(ByteBuffer buffer, int byteoffset){
+		int data = buffer.getInt(byteoffset);
+		return new Type.Int(data);
+	    }
+	}
     }
 }
