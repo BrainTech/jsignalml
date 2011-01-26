cd9ed173aeecf69816a13017de80813d13266380
Zbyszek Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-01-26 17:28:21 +0100

    Fix tests broken in recent BitForm restructuring.

--- src/test/java/jsignalml/TestFileType.java
+++ src/test/java/jsignalml/TestFileType.java
@@ -13,7 +13,7 @@ public class TestFileType {
 	@Test public void test_binary_file_bytes() throws Exception {
 	FileType.BinaryFile F = new FileType.BinaryFile(filename);
 
-		BitForm format = new BitForm.Int8();
+		BitForm format = new BitForm.Int.Int8();
 		assertThat(F.read(format, 0), equalTo((Type)new Type.Int('1')));
 		assertThat(F.read(format, 1), equalTo((Type)new Type.Int('2')));
 		assertThat(F.read(format, 2), equalTo((Type)new Type.Int('3')));
@@ -23,7 +23,7 @@ public class TestFileType {
 	@Test public void test_binary_file_int() throws Exception {
 	FileType.BinaryFile F = new FileType.BinaryFile(filename);
 
-		BitForm format = new BitForm.Int32.LE();
+		BitForm format = new BitForm.Int.Int32.LE();
 		int val = ((int)'4') + ((int)'3' << 8) +
 			((int)'2' << 16) + ((int)'1' << 24);
 		assertThat(F.read(format, 0), equalTo((Type)new Type.Int(val)));
@@ -33,7 +33,7 @@ public class TestFileType {
 	public void test_binary_file_read_after() throws Exception {
 		FileType.BinaryFile F = new FileType.BinaryFile(filename);
 
-		BitForm format = new BitForm.Int8();
+		BitForm format = new BitForm.Int.Int8();
 		F.read(format, 4);
 	}
 
@@ -41,7 +41,7 @@ public class TestFileType {
 	public void test_binary_file_read_before() throws Exception {
 		FileType.BinaryFile F = new FileType.BinaryFile(filename);
 
-		BitForm format = new BitForm.Int8();
+		BitForm format = new BitForm.Int.Int8();
 		F.read(format, -1);
 	}
 
@@ -49,14 +49,14 @@ public class TestFileType {
 	public void test_binary_file_read_at_edge() throws Exception {
 		FileType.BinaryFile F = new FileType.BinaryFile(filename);
 
-		BitForm format = new BitForm.Int32.LE();
+		BitForm format = new BitForm.Int.Int32.LE();
 		F.read(format, 2);
 	}
 
 	@Test public void test_binary_file_read_string() throws Exception {
 		FileType.BinaryFile F = new FileType.BinaryFile(filename);
 
-		BitForm format = new BitForm.STR(4);
+		BitForm format = new BitForm.Str(4);
 		Type s = F.read(format, 0);
 		assertThat(s, equalTo((Type)new Type.String("1234")));
 	}
