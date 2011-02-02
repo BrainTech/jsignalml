c9df434e77fbff6f79b3420f0a505d7505c015a2
Zbyszek Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-02-02 11:27:07 +0100

    Provide useful output in AST dump.

--- src/main/java/jsignalml/ASTDumper.java
+++ src/main/java/jsignalml/ASTDumper.java
@@ -14,6 +14,23 @@ public class ASTDumper extends ASTVisitor<Integer> {
 			this.buffer.append("    ");
 	}
 
+	private void put(int level, String fmt, Object...args)
+	{
+		this.indent(level + 1);
+		this.buffer.append(format(fmt + "\n", args));
+	}
+
+	private void header(int level, ASTNode node, String display,
+			    String extrafmt, Object...args)
+	{
+		this.indent(level + 1);
+		this.buffer.append
+			(format("%s [%s] %s ", display, node.id,
+				node.parent == null ? "no parent"
+				: format("parent=[%s]", node.parent.id)));
+		this.buffer.append(format(extrafmt + "\n", args));
+	}
+
 	public static String dump(ASTNode node)
 	{
 		final ASTDumper dumper = new ASTDumper();
@@ -24,56 +41,53 @@ public class ASTDumper extends ASTVisitor<Integer> {
 	@Override
 	public Integer visit(ASTNode.Signalml node, Integer parent)
 	{
-		this.indent(parent + 1);
-		this.buffer.append(format("signalml\n"));
+		this.header(parent, node, "signalml", "");
 		return parent + 1;
 	}
 
 	@Override
 	public Integer visit(ASTNode.BinaryParam node, Integer parent)
 	{
-		this.indent(parent + 1);
-		this.buffer.append(format("param (read binary)\n"));
+		this.header(parent, node, "param (read binary)", "");
+		this.put(parent + 1, "* format=%s", node.format);
+		this.put(parent + 1, "* offset=%s", node.offset);
 		return parent + 1;
 	}
 
 	@Override
 	public Integer visit(ASTNode.ExprParam node, Integer parent)
 	{
-		this.indent(parent + 1);
-		this.buffer.append(format("param (expression)\n"));
+		this.header(parent, node, "param (expression)", "");
+		this.put(parent + 1, "* expression=%s", node.expr);
 		return parent + 1;
 	}
 
 	@Override
 	public Integer visit(ASTNode.Assert node, Integer parent)
 	{
-		this.indent(parent + 1);
-		this.buffer.append(format("assert\n"));
+		this.header(parent, node, "assert", "");
+		this.put(parent + 1, "* expression=%s", node.expr);
 		return parent + 1;
 	}
 
 	@Override
 	public Integer visit(ASTNode.FileHandle node, Integer parent)
 	{
-		this.indent(parent + 1);
-		this.buffer.append(format("file\n"));
+		this.header(parent, node, "file", "filename=%s", node.filename);
 		return parent + 1;
 	}
 
 	@Override
 	public Integer visit(ASTNode.DataHandle node, Integer parent)
 	{
-		this.indent(parent + 1);
-		this.buffer.append(format("data\n"));
+		this.header(parent, node, "data", "");
 		return parent + 1;
 	}
 
 	@Override
 	public Integer visit(ASTNode.Positional node, Integer parent)
 	{
-		this.indent(parent + 1);
-		this.buffer.append(format("arg\n"));
+		this.header(parent, node, "arg", "type=%s", node.type);
 		return parent + 1;
 	}
 }
