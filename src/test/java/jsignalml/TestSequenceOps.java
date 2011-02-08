87919c010e0e6eeddfe81b42282daba7c5748160
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-02-08 20:58:08 +0100

    Add tests for negative indexes.

--- src/test/java/jsignalml/TestSequenceOps.java
+++ src/test/java/jsignalml/TestSequenceOps.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import static jsignalml.TestNumberOps.eval;
 import static jsignalml.TestNumberOps.equal;
 import static jsignalml.TestNumberOps.listEqual;
 
@@ -25,5 +26,50 @@ public class TestSequenceOps {
 	@Test public void eval_simple_list() throws Exception
 	{
 		listEqual("[1, 2]", 1, 2);
+	}
+
+	@Test public void eval_index_string_from_end() throws Exception
+	{
+		equal("'string'[-1]", "g");
+		equal("'string'[-4]", "r");
+	}
+
+	@Test public void eval_index_list_from_end() throws Exception
+	{
+		equal("[0,1,2][-1]", 2);
+		equal("[0,1,2][-2]", 1);
+		equal("[0,1,2][-3]", 0);
+	}
+
+	@Test(expected=ExpressionFault.IndexError.class)
+	public void eval_index_invalid_1() throws Exception
+	{
+		eval("[0, 1][2]");
+	}
+	@Test(expected=ExpressionFault.IndexError.class)
+	public void eval_index_invalid_2() throws Exception
+	{
+		eval("[0][1]");
+	}
+	@Test(expected=ExpressionFault.IndexError.class)
+	public void eval_index_invalid_3() throws Exception
+	{
+		eval("[0][-2]");
+	}
+
+	@Test(expected=ExpressionFault.IndexError.class)
+	public void eval_index_str_invalid_1() throws Exception
+	{
+		eval("'ab'[2]");
+	}
+	@Test(expected=ExpressionFault.IndexError.class)
+	public void eval_index_str_invalid_2() throws Exception
+	{
+		eval("'a'[1]");
+	}
+	@Test(expected=ExpressionFault.IndexError.class)
+	public void eval_index_str_invalid_3() throws Exception
+	{
+		eval("'a'[-2]");
 	}
 }
