7e2062f18094d4f7ab80b67e254477b165fd9e46
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-04-08 19:34:08 +0200

    Fix vertical whitespace in tree dumps.

--- src/main/java/jsignalml/TreeDumper.java
+++ src/main/java/jsignalml/TreeDumper.java
@@ -7,6 +7,9 @@ package jsignalml;
 public class TreeDumper {
 	final StringBuilder buffer = new StringBuilder();
 
+	/**
+	 * Add indentation of level times "    " at current point.
+	 */
 	private void indent(int level)
 	{
 		for (int indent = 0; indent < level; indent++)
@@ -16,12 +19,12 @@ public class TreeDumper {
 	public void put(int level, String fmt, Object...args)
 	{
 		this.indent(level + 1);
-		this.buffer.append(String.format(fmt + "\n", args));
+		this.buffer.append(String.format(fmt, args));
 	}
 
 	public void format(String fmt, Object...args)
 	{
-		this.buffer.append(String.format(fmt + "\n", args));
+		this.buffer.append(String.format(fmt, args));
 	}
 
 	public String getText()
