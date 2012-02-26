37e07f3bb432cddc2686fd878773e60be4dcf84e
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2012-02-26 21:55:41 +0100

    testdumper: document invocation and usage

--- src/main/java/jsignalml/TestFormatDumper.java
+++ src/main/java/jsignalml/TestFormatDumper.java
@@ -9,6 +9,53 @@ import org.apache.commons.io.FilenameUtils;
 
 import jsignalml.codec.Signalml;
 
+/**
+ * Dumps data from a data file, using a Signalml codec.
+ * <p>
+ * Sample values are written on stdout, as space seperated values, one sample
+ * (<i>n</i> channels), per output line:
+ * <pre>
+ *    ch1-sample1 ch2-sample1 ch3-sample1 ... chn-sample1
+ *    ch1-sample2 ch2-sample2 ch3-sample2 ... chn-sample2
+ *    ...
+ *    ch1-samplem ch2-samplem ch3-samplem ... chn-samplem
+ * </pre>
+ * <p>
+ * Metadata are written to <tt>&lt;filename-minus-extension&gt;.hdr</tt> in
+ * current directory. The format is
+ * <ul>
+ *   <li><tt>field = value</tt>
+ * </ul>
+ * or
+ * <ul>
+ *   <li><tt>field = value1, value2, value3, ...</tt>
+ * </ul>
+ * for per-channel values.
+ * <p>
+ * The following fields should be present:
+ * <ul>
+ *   <li><tt>number_of_samples</tt>
+ *   <li><tt>number_of_channels</tt>
+ *   <li><tt>sampling_frequency</tt>
+ *   <li><tt>channel_labels</tt>
+ * </ul>
+ * <p>
+ * If the signal is scaled by the codec <tt>scaled = 1</tt> is added.
+ * If the signal is not scaled, the following fields are added:
+ * <ul>
+ *   <li><tt>scaled = 0</tt>
+ *   <li><tt>scaling_gain</tt>
+ *   <li><tt>scaling_offset</tt>
+ * </ul>
+ * 
+ * To generate data in the "test format", the following invocation can be used:
+ * <pre>
+ *   java jsignalml.TestFormatDumper &lt;codec-name&gt; &lt;data-file&gt;.&lt;ext&gt; |
+ *          xz -9v &gt; &lt;data-file&gt;.ascii.xz
+ * </pre>
+ * This should generate <tt>.hdr</tt> and <tt>.ascii.xz</tt> files in current
+ * directory.
+ */
 public class TestFormatDumper {
 	static Source makeSource(String klass_name, String file)
 		throws Exception
