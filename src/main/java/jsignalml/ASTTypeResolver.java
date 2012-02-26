c07fa83f17b826ae0d4d2816285ac1c0dc6176be
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2012-02-26 21:56:11 +0100

    doc: fix docstring format

--- src/main/java/jsignalml/ASTTypeResolver.java
+++ src/main/java/jsignalml/ASTTypeResolver.java
@@ -5,7 +5,7 @@ public interface ASTTypeResolver {
 	 * Tell what type the node is. In case of ParamNodes this is the result
 	 * of the expression or the type of data read based upon the format.
 	 *
-	 * @returns Type or null iff not known
+	 * @return Type or null iff not known
 	 */
 	Type getType(ASTNode node);
 }
