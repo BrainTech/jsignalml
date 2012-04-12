405198d4ed4c4dc20f50f1cd2b02b6d9122bb8d8
Kamil Góźdź <kamil.gozdz@ericpol.com>
2012-04-12 09:58:36 +0200

    minor update of the test case

--- src/test/java/jsignalml/TestParameterized.java
+++ src/test/java/jsignalml/TestParameterized.java
@@ -27,7 +27,7 @@ public class TestParameterized {
  
 	@Parameters
 	public static Collection<Object[]> data() {
-		Object[][] data = new Object[][] { { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 } };
+		Object[][] data = new Object[][] { { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, decrease(5) } };
 		return Arrays.asList(data);
 	}
  
@@ -35,5 +35,9 @@ public class TestParameterized {
 	public void pushTest() {
 		System.out.println("Parameterized Number is : " + number);
 		assertEquals(expected, number);
+	}
+
+	private static int decrease(int num){
+		return num-1;
 	}
 }
